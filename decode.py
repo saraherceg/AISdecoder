d7a358c7b3a16c8b29d12c19c56c2246565bd35d
tmv200 <t.marjanov@student.vu.nl>
2018-04-15 15:56:50 +0200

    Update decode.py

--- decode.py
+++ decode.py
@@ -4,14 +4,48 @@ import ais
 import os
 import time
 
+from mpl_toolkits.basemap import Basemap
+import numpy as np
+import matplotlib.pyplot as plt
+
 decoded_observations = 0
 all_observations = 0
+file_counter = 0
+plot_counter = 0
+
+print ("Creating a map")
+
+map = Basemap(projection='merc', llcrnrlat=0, urcrnrlat=60, llcrnrlon=-40, urcrnrlon=40, lat_ts=10, resolution='i')
+map.drawcoastlines()
+map.shadedrelief()
+map.drawparallels(np.arange(-90.,91.,30.))
+map.drawmeridians(np.arange(-180.,181.,60.))
+plt.title("Observations map")
+
+def plot_on_map(observation):
+	global map
+	global plot_counter
+	x = 0
+	y = 0
+	#print(observation)
+	observation_list = observation.split(", ")
+	for i in observation_list:
+		if i.startswith("u'y'"):
+			y_coordinate = i.split(": ")
+			y = float(y_coordinate[1])
+			#print y
+		if i.startswith("u'x'"):
+			x_coordinate = i.split(": ")
+			x = float(x_coordinate[1])
+			#print x
+	if (x != 0 and y != 0): 
+		lon, lat = map(x,y)
+		map.plot(lon,lat,'ro', markersize = 2)
+		plot_counter = plot_counter + 1
 
 def decode(filename):
 
 	file  = gzip.open(filename, 'r')
-	output = open("decoded.txt", "a")
-
 	global decoded_observations
 	global all_observations
 	timestamp = '0'
@@ -20,31 +54,33 @@ def decode(filename):
 		all_observations = all_observations +1
 		try:
 			line_list = line.split(",")
-			field_0 = line_list[0]
 
+			field_0 = line_list[0]
 			if(field_0[0] != "!"):
 				# timestamp is present
 				timestamp = field_0[:17]
 			field_6 = line_list[6].rstrip('\r\n')
-
 			if (str(field_6)[0] == "0"):
 				decoded = str((ais.decode(str(line_list[5]), 0)))
 				## insert current timestamp value
 				timestamp_position = (decoded).find("timestamp") + 12
 				decoded_timestamp = decoded[:timestamp_position] + str(timestamp) + decoded[timestamp_position+3:]
-	 			output.write(decoded_timestamp+ '\n\n')
-				decoded_observations = decoded_observations +1
+				if (all_observations%10 == 0):	#specify if want to only run through a part
+					plot_on_map(decoded_timestamp)
+					decoded_observations = decoded_observations+1	
 			else:
 				file.readline()      # skip next line as it is a part of previous observation
-		except Exception: 
+		except Exception:
 			pass
+	
 	print ("Successfully decoded observations: " + str(decoded_observations) +"/" + str(all_observations) + " (" + str(float(decoded_observations)/float(all_observations)) + ")")	
 
-
 start = time.time()
+
 for filename in os.listdir(os.getcwd()):
 	if filename.endswith(".txt.gz"):
-		print ("decoding " + filename)
+		file_counter = file_counter+1
+		print ("decoding file " + str(file_counter) +": " + filename )
 		decode(filename)
 		continue
 	else:
@@ -52,4 +88,6 @@ for filename in os.listdir(os.getcwd()):
 
 end = time.time()
 print("Time elapsed: " + str(end - start))
-print("The data has been decoded and stored in decoded.txt")
+print ("Successfully decoded observations: " + str(decoded_observations) +"/" + str(all_observations) + " (" + str(float(decoded_observations)/float(all_observations)) + ")")
+print ("Observations plotted on the map: " + str(plot_counter))
+plt.show()
